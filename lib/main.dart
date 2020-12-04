13a8dbc37133b63854ae92c66ca7fef5e91161bb
shyam taparia <shyamtap2405@gmail.com>
2020-12-04 22:49:53 +0530

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
-import 'package:agri/screens/image_picker_screen.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -21,19 +27,158 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _storedImage;
+  final picker = ImagePicker();
+  String predict ;
+
+  Future imageFromCamera() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (imageFile != null) {
+        _storedImage = File(imageFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+  }
+
+  Future imageFromGallery() async {
+    File imageFile = await ImagePicker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 50,
+        maxHeight: 300,
+        maxWidth: 300);
+
+    setState(() {
+      if (imageFile != null) {
+        _storedImage = File(imageFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Gallery'),
+                      onTap: () {
+                        imageFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      imageFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future _submitData() async {
+    String url = " ";
+    final response =
+        await http.post(url, headers: {"content/type": "application/json"});
+    final extractedData = jsonDecode(response.body);
+  setState(() {
+    predict = extractedData['prediction'];
+  });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Agriculture'),),
+      appBar: AppBar(
+        title: Text('Agriculture'),
+      ),
       body: Container(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ImagePickerScreen(),
-              RaisedButton(onPressed: (){},child: Text('check for disease',style: TextStyle(color: Colors.white),),color: Theme.of(context).accentColor,)
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 120,
+                  ),
+                  Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey)),
+                    child: _storedImage != null
+                        ? SingleChildScrollView(
+                            child: Image.file(
+                              _storedImage,
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          )
+                        : Center(
+                            child: Text(
+                              'No image taken',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FlatButton.icon(
+                      onPressed: () {
+                        _showPicker(context);
+                      },
+                      icon: Icon(
+                        Icons.camera,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      label: Text(
+                        'Take picture',
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ))
+                ],
+              ),
+              SizedBox(height:2,),
+              
+              predict!= null ? Container(child: Column(
+                children: [Container(child: Text("Disease Predicted:", style: TextStyle(fontSize: 20,color: Colors.red),)),
+                  SizedBox(height: 10,),
+                  Center(child: Text(predict,style:TextStyle(fontSize: 14,fontWeight: FontWeight.bold))),
+                ],
+              )): Container(),
+              RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  'check for disease',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Theme.of(context).accentColor,
+              ),
+              
             ],
           ),
         ),
